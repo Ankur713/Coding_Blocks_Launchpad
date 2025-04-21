3aa1383afc39f10524d130a6ada5f3c6bf9cfcd1
Ankur bhadauria <97403637+Ankur713@users.noreply.github.com>
2025-04-21 19:54:06 +0530

    Add files via upload

---
+++ Lecture02/Code/c.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;  
+
+
+
+int main(){
+    
+ 
+    //types of variables
+    // char, int , string, boolean , array 
+    // custom data types  classes, struct
+    
+    // declare of variables
+    // int var;
+    // char chr;
+    // string st;
+    // int a[10];-
+    
+    // input and output 
+    // (cin >>) and (cout <<)
+    
+    // int a;
+    // cin >> a;
+    // cout << a ;
+
+    // char  ch; cin >> ch;
+    // loops 
+    // for loop 
+
+    // for(int i = 10; i>0;i--){
+
+    // }
+    // while loopp
+    // int i =0;
+    // while(i<10){
+
+    //     i++;
+    // }
+    // do while 
+    //  1
+    //  +, - *, / %,
+
+    // string a ="700     // string b = "70";
+    // if(a > b){
+    //     cout <<"a is greater";
+    // }
+    // else if(a == b) cout << "both are equal";
+    // else {
+    //     cout << "b is greater";
+    // }
+
+
+
+    // int cnt = 1;
+    // for(int i=1;i<=4;i++){
+    //     for(int j=1;j<=i;j++){
+    //         cout <<cnt;
+    //         cnt++;
+    //     }
+    //     cout << endl;
+    // }
+    // 
+   // int x = 2;
+   // x++; // x = 3;
+
+    // cout << x + (x++) + (++x) + x;
+          //  2    3       4     4 
+}
