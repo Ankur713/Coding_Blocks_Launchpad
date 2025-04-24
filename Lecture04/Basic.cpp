253f4591d321e6509871afb92c8d7d9f5ac2f6ef
Ankur bhadauria <97403637+Ankur713@users.noreply.github.com>
2025-04-24 16:01:54 +0530

    Add files via upload

---
+++ Lecture04/Basic.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    // Prime no between 2 to n 
+    // int n; cin >> n;
+    // for(int i=2;i<=n;i++){
+        
+    //     int cnt = 0;
+    //     for(int j=1;j<=n;j++) if(i%j==0)cnt++;
+        
+    //     if(cnt == 2) cout << i <<" ";
+    // }
+    int x = 9;
+    int t = (2/9.0)*x;
+
+    cout << t ;
+    
+    return 0;
+}
