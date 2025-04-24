db925afa244ade58504df4f2ad1217cb74e28c70
Ankur bhadauria <97403637+Ankur713@users.noreply.github.com>
2025-04-24 16:22:05 +0530

    Create Reverse a no.cpp

---
+++ Lecture05/Reverse a no.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n,r=0;
+    cin>>n;
+  
+    while(n > 0) { 
+      r=r*10+n%10;
+      n/=10;
+    }
+    cout<<r;
+
+  return 0;
+}
