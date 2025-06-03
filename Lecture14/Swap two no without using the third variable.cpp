b84ad21fc9ac2d37271910b4d19066198b283f49
Ankur bhadauria <97403637+Ankur713@users.noreply.github.com>
2025-06-03 10:08:42 +0530

    Create Swap two no without using the third variable.cpp

---
+++ Lecture14/Swap two no without using the third variable.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    
+    int a,b; cin >> a >> b;
+    // 1 by using third variable
+    // a = a+b;
+    // b = a-b;
+    // a = a-b;
+    // cout << a << " " << b << endl;
+    
+    // swap(a,b);
+    // cout << a <<" " << b << endl;
+    
+    // a = a ^ b; 
+    // b = b ^ a;   b^a^b  => b = a;
+    // a = a ^ b;   a^b^a=>  a = b;
+    
+    // cout << a <<" " << b << endl;
+    
+    
+    
+    
+    return 0;
+}
