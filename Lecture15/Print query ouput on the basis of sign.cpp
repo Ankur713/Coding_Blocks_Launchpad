b5c122625b5b91e79546becb3eef31525e3704d8
Ankur bhadauria <97403637+Ankur713@users.noreply.github.com>
2025-06-04 11:02:08 +0530

    Create Print query ouput on the basis of sign.cpp

---
+++ Lecture15/Print query ouput on the basis of sign.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+
+int a[26];
+
+// if you declare any array of contant size 
+// element already to 0
+  
+int main() 
+{  
+  
+  // INPUT:
+  // N 
+  // 1----------N 
+  // CHAR , SIGN , VALUE
+  // K 
+  // 1-----K 
+  // CHAR 
+  
+  // OUPUT: char -> total value
+  int n; 
+  cin >> n;
+  
+  for(int i=1;i<=n;i++){
+    char ch,sign;
+    cin >> ch >> sign;
+    
+    int value;
+    cin >> value;
+    
+    if(sign =='+') a[ch-'a'] += value;
+    else  a[ch-'a'] -= value;
+  }
+  int k;
+  cin >> k;
+  
+  for(int i=1;i<=k;i++){
+    char ch;
+    cin >> ch;
+    cout << a[ch-'a'] << endl;
+  }
+  
+  
+  
+  return 0;
+}
