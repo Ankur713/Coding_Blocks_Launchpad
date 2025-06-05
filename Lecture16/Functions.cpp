b88047f6817f78503c7f835de54774c90bd3170e
Ankur bhadauria <97403637+Ankur713@users.noreply.github.com>
2025-06-05 10:40:52 +0530

    Create Functions.cpp

---
+++ Lecture16/Functions.cpp
@@ -0,0 +1,149 @@
+#include <iostream>
+using namespace std;
+
+// Recursion 
+// whenever a funtion call itself 
+
+
+// function declaration
+// data_type funtion_name(no of argument) {
+  
+// }
+
+// data types in funtion 
+// argument => these are values given to the funtion for further computations
+
+//variables
+
+// int funtion_name (){
+   
+//   cout << "456" << endl;
+   
+//   // return 1;
+// }
+
+ // we have to create a function for adding two no's 
+
+// void add(int x, int y){
+//   cout << x+y << endl;
+// }
+//
+ //Create a function for printing the table of given no  till 10;
+
+// Function overloading
+
+
+// void printing(int k){
+  
+//   for(int i=1;i<=10;i++){
+//     cout << k <<"*" << i <<":" << i*k << endl;
+//   }
+// }
+
+
+// void printing(int x ,int y){
+//   cout <<"THIS IS THE VALUE OF X" << x << endl;
+// }
+
+
+
+// Write a funtion to check my no is even or odd
+
+// void evenorodd( int t) {
+//     //int t = 9;
+//     if(t%2 == 0) cout <<"EVEN";
+//     else cout <<"ODD";
+// }
+    
+// Write a funtion to calculate the multiplication of two no's
+// input is given my user
+
+
+// void mul(int x, int y){
+  
+//   cout << x*y << endl;
+// }
+
+ //Write a funtion to check my given no is prime or not, factors is equal to 2 
+
+// void primeno(int n){
+//     int factors = 0;
+//     for(int i=1;i<=n;i++){
+//       if(n%i ==0) factors++;
+//     }
+//   //  cout << factors << endl;
+//     if(factors <=2){
+//       cout <<"Given no is my prime no";
+//     }
+//     else  cout <<"Give no is not prime no";
+  
+// }
+
+
+// void function_name (map<int,int> x, float xt){
+  
+//   //return ""
+// }
+  
+// int x (){
+//   return 0;
+// }
+
+// string x() {
+//   return "";
+// }
+
+// bool x(){
+  
+//   return false;
+// }
+
+// double () {
+//   return 9.2;
+// }
+// float () {
+//   return 930.33;
+// }
+
+// vector<int>  x () {
+   
+//   vector<int> t;
+//   return   t;
+// }
+
+
+// map<int,int> x(arguments1, argument2, ....) {
+  
+//   map<int,int> xt;
+  
+//   return xt;
+// }
+
+// void x(){
+  
+// }
+
+
+int main() {
+  
+    // int x = 5;
+    // double t = 5.0;
+    
+    // printing(x);
+    
+    // printing(2,4);
+    
+     //evenorodd(9);
+    // int first , second;
+    // cin >> first >> second;
+    // mul(first, second);
+    int n;
+    cin >> n;
+    primeno(n);
+   // cout << n << endl;
+  
+    return 0;
+    
+}
+
+
