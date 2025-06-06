94b5c52b995b299d1ae8a9cb5bee2e4e2ad18371
Ankur bhadauria <97403637+Ankur713@users.noreply.github.com>
2025-06-06 22:06:58 +0530

    Create What is auto keyword.cpp

---
+++ Lecture17/What is auto keyword.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+
+  // for(int i=0;i<n;i++) cout << a[i];
+  
+  // for(auto i:a) cout << i;
+  // why we need auto  
+  // easy to use 
+  int n;
+  cin >> n;
+  cout << n << endl;
+  // In case of array
+  // a= [1,2,3,4,5,6]
+  //     i i i i i i
+  // // i => iterator point to the each element 
+  // for(auto i:a) cout << i << endl;
+   
+  // In case of String
+  // s = "acededgded"
+  //     iiii
+  // for(auto i:s) cout << i << endl;
+  
+  // In case of  map  
+  // mp = { {1,1},{2,4},{5,7}};
+  //         i    i      i 
+  //         i.first 
+  //         i.second
+  // for(auto i:mp) 
+   //cout << a[i] << endl;
+  
+  // for auto i in a
+  //auto for finding the datatype automatically
+    
+    return 0;
+}
