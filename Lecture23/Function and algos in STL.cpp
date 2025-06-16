bd24bd146138679104b1ae528e24c0aa829df282
Ankur bhadauria <97403637+Ankur713@users.noreply.github.com>
2025-06-16 10:18:23 +0530

    Create Function and algos in STL.cpp

---
+++ Lecture23/Function and algos in STL.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function and algos in STL 
+// Standard template library
+
+int main() 
+{
+    
+  // Max function 
+  cout << max(5,10) << endl;
+  // Min funtion 
+  cout << min(5,10) << endl;
+  // SQRT function
+  cout << sqrt(64) << endl;
+  // LOG function
+  cout << log(2) << endl;
+  // Round funtion 
+  cout << round(2.5) << endl;
+  // Floor Function 
+  cout << floor(3.9) << endl;
+  // ceil funtion 
+  cout << ceil(3.5) << endl;
+  // power function 
+  cout << pow(2,5) << endl;
+  // abs function 
+  cout << abs(-29) << endl;
+  // swap function 
+  int a = 10;
+  int b= 20;
+  swap(a,b);
+  cout << a <<" " << b << endl;
+  // tolower function 
+  cout << char(tolower('F')) << endl; // typecasting explicit forcefully
+  // toupper funtion 
+  cout << char(toupper('a')) << endl;
+  // gcd function 
+  cout << __gcd(10,5) << endl;
+  //  __builtin_popcount(n)=> Find the no of setbits in our binary no 
+  cout << __builtin_popcount(5) << endl;
+  // gcd(a,b)*lcm(a,b) = a*b;
+  
+  
+  
+  
+  
+    
+    return 0;
+}
