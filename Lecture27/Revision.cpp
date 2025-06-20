efd23db034662d72eda4a42c567c022356017488
Ankur bhadauria <97403637+Ankur713@users.noreply.github.com>
2025-06-20 11:00:49 +0530

    Create Revision.cpp

---
+++ Lecture27/Revision.cpp
@@ -0,0 +1,160 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// how to declare a variable
+// Arrays, strings 
+// Vectors, Map, set 
+// funtion 
+
+int main() 
+{
+  // Variable 
+  // Datatype nameofvariable;
+  
+  // int x = 10;
+  // char ch = 'F';
+  // boolean flag = true;
+  // float ft = 3.4455 // upto 7 digit after decimal
+  // double t = 3.45433433 // upto 15 digits after decimal 
+  
+  // // Array 
+  
+  // int ar[10];
+  // int a[3] = {1,2,3};
+  // a[0] = 1;
+  // a[0] = 34;
+  
+  // for(int i=0;i<n;i++) cin >> a[i];
+  
+  // // Reverse the array 
+  // for(int i=0;i<n/2;i++) swap(a[i],a[n-i-1]);
+  
+  // // Find the pair of element having sum equal to my target 
+  // for(int i=0;i<n;i++){
+  //   for(int j=i+1;j<n;j++){
+  //     if(a[i] + a[j] == target) {
+  //       cout << a[i] << a[j] << endl;
+       
+  //     }
+  //   }
+  // }
+  // // Print the count of each element 
+  // int b[n+1];
+  // for(int i=0;i<n;i++){
+  //   int curr = a[i];
+  //   b[a[i]]++;
+  // }
+  // for(auto i:b) cout << i <<" ";
+  
+  // // xor operator 
+  // 1^1 = 0 
+  // 0^0 = 0 // xor of same no's would we 0 
+  // xor of any no occuring even no of time will also be 0 
+  
+  // int n; 
+  // cin >> n;
+  
+  // int a[n];
+  // for(int i=0;i<n;i++) cin >> a[i];
+  
+  // int xo = a[0];
+  // for(int i=1;i<n;i++) xo = xo^a[i];
+  
+  // cout << xo << endl;
+  
+  // string s;
+  // cin >> s;
+  
+  // substr()  it returns me a substring 
+//  1  s.substr(a) from ath index till last 
+ // 2  s.substr(a,b); from ath and coun the b chars from the ath index
+  
+// cout << s.substr(2) << endl;
+// cout << s.substr(2,2) << endl;
+
+  // char ch = s[0];
+  // for(int i=1;i<s.size();i++) ch = ch^s[i];
+  // cout << ch << endl;
+  
+  // Vectors
+  // 1 declaration
+  // vector<int> vtt;  // empty vector
+  // vector<int> v(10); // vector of size 10 and value garbage value
+  // vector<int> vt(10,8); // vector of size 10 and each value 8
+  
+  // // 2 value push 
+  // for(int i=0;i<10;i++) cin >> v[i];
+  
+  // vtt.push_back(10);
+  // vtt.push_back(2);
+  
+  
+  // int a[10];
+  // for(int i=0;i<10;i++) cin >> a[i];
+  
+  // vector<int>v (10);
+  // for(int i=0;i<10;i++) cin >> v[i];
+  // v.push_back(23);
+  // //3 pop back
+  // v.pop_back();
+  
+  // MAP OG 
+//   map<char,int> t;
+// //  key-value 
+// // insert
+//   string s; 
+//   cin >> s;
+  
+
+  
+//   for(int i=0;i<s.size();i++){
+//     char curr = s[i];
+//     t[cur]++;
+//   }
+  
+// // output
+// // 2->3  
+// // 4->5 
+// // 6->2
+// // first  second
+ 
+//   for(auto i:t){
+//     if(i.second == 2)
+//     cout << i.first <<"->" << i.second << endl;
+//   }
+   
+  // string s;
+  // //cin >> s;
+  // getline(cin, s);
+  
+  // cout << s << endl;
+// Particular key - value
+              // int -  int 
+              // int -  char
+              // string - char
+              
+ // type casting 
+// change in the datatype of any value during comp
+// complier , user 
+// implict    explicit forcefully
+// 
+  // double temp = int(5/2.0);
+  // cout << temp << endl;
+  //               0
+  // double xt = (7/22)*(100+100);
+  // cout << xt << endl;
+ // cout << 7/22<< endl;
+  
+  
+
+  return 0;
+}
+
+
+
+
+
+
+
+
+
