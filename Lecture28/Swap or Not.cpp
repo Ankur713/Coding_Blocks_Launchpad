4b74104c0824603e0687ba9dd1740010b1d6beee
Ankur bhadauria <97403637+Ankur713@users.noreply.github.com>
2025-06-24 10:43:18 +0530

    Create Swap or Not.cpp

---
+++ Lecture28/Swap or Not.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+
+// returntype funtioname(argument1, arugment2){
+  
+// }
+
+
+
+
+void solve(int &x, int &y) {
+  
+  swap(x,y);
+  
+}
+
+
+int main() 
+{
+  
+  int x = 10,y = 20;
+  solve(x,y);
+  cout << x << " " << y << endl;
+
+  return 0;
+}
+
