3ba50ed318bdb6269dbdcade4ea956a329ee4707
Ankur bhadauria <97403637+Ankur713@users.noreply.github.com>
2025-06-24 10:29:26 +0530

    Create Find the count of element using recursion.cpp

---
+++ Lecture28/Find the count of element using recursion.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int cnt;
+int target;
+int n;
+
+void solve(int index, int a[]) {
+   // base case
+   if(index > n-1) return;
+   // operation 
+   if(a[index] == target) cnt++;
+   // recursive call
+   solve(index+1, a);
+}
+
+int main() 
+{
+  
+
+  cin >> n;
+
+  int a[n];
+  for(int i=0;i<n;i++) cin >> a[i];
+  
+  cin >> target;
+  solve(0,a);
+  
+  cout << cnt << endl;
+  
+  return 0;
+}
